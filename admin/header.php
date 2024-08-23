d25bbc74323f1210e7633ff0d3366d5cf58b04f4
Andika Prananto <andika.prananto@dagangan.com>
2024-08-24 02:03:38 +0700

    "Updated dropdown header to display user's first name from session"

--- admin/header.php
+++ admin/header.php
@@ -123,7 +123,7 @@
                     </button>
                     <div class="dropdown-menu dropdown-menu-end">
                         <!-- item-->
-                        <h6 class="dropdown-header">Welcome Anna!</h6>
+                        <h6 class="dropdown-header">Welcome <?php echo explode(' ', $_SESSION['nama'])[0]; ?>!</h6>
                         <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Profile</span></a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="pages-profile-settings.html">
