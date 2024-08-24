bda4c69ec53690c2f802ab8b73104de3bcbbc04e
Andika Prananto <andika.prananto@dagangan.com>
2024-08-24 21:30:39 +0700

    "Updated admin sidebar.php: Renamed 'Management User' to 'Pages', added 'Data Category' and 'Data Transaction' menus, and rearranged menu items."

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -10,30 +10,70 @@
                             </a>
                         </li> <!-- end Dashboard Menu -->
 
-                        <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-components">Management User</span></li>
-
-                        <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarUI" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarUI">
-                                <i class="ri-pencil-ruler-2-line"></i> <span data-key="t-base-ui">Users</span>
-                            </a>
-                            <div class="collapse menu-dropdown mega-dropdown-menu" id="sidebarUI">
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <ul class="nav nav-sm flex-column">
-                                        <li class="nav-item">
-                                                <a href="user.php" class="nav-link" data-key="t-alerts">List Users</a>
-                                            </li>
+                        <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-components">Pages</span></li>
+                            <li class="nav-item">
+                                <a class="nav-link menu-link" href="#sideCategory" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sideCategory">
+                                    <i class="ri-pencil-ruler-2-line"></i> <span data-key="t-base-ui">Data Category</span>
+                                </a>
+                                <div class="collapse menu-dropdown mega-dropdown-menu" id="sideCategory">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <ul class="nav nav-sm flex-column">
                                             <li class="nav-item">
-                                                <a href="user_tambah.php" class="nav-link" data-key="t-alerts">Add Users</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="gantipassword.php" class="nav-link" data-key="t-alerts">Changes Password</a>
-                                            </li>
-                                        </ul>
+                                                    <a href="" class="nav-link" data-key="t-alerts">List Category</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="" class="nav-link" data-key="t-alerts">Add Category</a>
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </li>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link menu-link" href="#sideTransaction" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sideTransaction">
+                                    <i class="ri-pencil-ruler-2-line"></i> <span data-key="t-base-ui">Data Transaction</span>
+                                </a>
+                                <div class="collapse menu-dropdown mega-dropdown-menu" id="sideTransaction">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <ul class="nav nav-sm flex-column">
+                                            <li class="nav-item">
+                                                    <a href="" class="nav-link" data-key="t-alerts">List Transaction</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="" class="nav-link" data-key="t-alerts">Add Transaction</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            
+
+                        <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-components">Management User</span></li>
+                            <li class="nav-item">
+                                <a class="nav-link menu-link" href="#sidebarUI" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Users">
+                                    <i class="ri-pencil-ruler-2-line"></i> <span data-key="t-base-ui">Users</span>
+                                </a>
+                                <div class="collapse menu-dropdown mega-dropdown-menu" id="sidebarUI">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <ul class="nav nav-sm flex-column">
+                                            <li class="nav-item">
+                                                    <a href="user.php" class="nav-link" data-key="t-alerts">List Users</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="user_tambah.php" class="nav-link" data-key="t-alerts">Add Users</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="gantipassword.php" class="nav-link" data-key="t-alerts">Changes Password</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
                     </li>
                 </div>
                 <!-- Sidebar -->
