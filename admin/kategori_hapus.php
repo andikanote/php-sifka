affee968e2fececa9976658c01c3f3b74ad4051e
Andika Prananto <andika.prananto@dagangan.com>
2024-08-24 23:33:03 +0700

    "Updated kategori_hapus.php to add query parameter to redirect location"

--- admin/kategori_hapus.php
+++ admin/kategori_hapus.php
@@ -5,4 +5,4 @@ $id  = $_GET['id'];
 mysqli_query($koneksi, "update transaksi set transaksi_kategori='1' where transaksi_kategori='$id'");
 
 mysqli_query($koneksi, "delete from kategori where kategori_id='$id'");
-header("location:kategori.php");
+header("location:kategori.php?alert=hapus");
