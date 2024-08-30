f1369e5ac64889c5bf89f2dd6c06f5a6cc60a7b4
Andika Prananto <andika.prananto@dagangan.com>
2024-08-30 21:09:55 +0700

    "Updated no result found messages in admin and manajemen pages"

--- admin/kategori.php
+++ admin/kategori.php
@@ -220,7 +220,7 @@
                                         <div class="text-center">
                                             <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px"></lord-icon>
                                             <h5 class="mt-2">Sorry! No Result Found</h5>
-                                            <p class="text-muted mb-0">We've searched Category We did not find any orders for you Category.</p>
+                                            <p class="text-muted mb-0">We've searched for the data you want, but we didn't find it for your search. Category We did not find any orders for you Category.</p>
                                         </div>
                                     </div>
                                 </div>
--- admin/user.php
+++ admin/user.php
@@ -172,7 +172,7 @@
                                                 <div class="text-center">
                                                     <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px"></lord-icon>
                                                     <h5 class="mt-2">Sorry! No Result Found</h5>
-                                                    <p class="text-muted mb-0">We've searched user We did not find any user for you search.</p>
+                                                    <p class="text-muted mb-0">We've searched for the data you want, but we didn't find it for your search. user We did not find any user for you search.</p>
                                                 </div>
                                             </div>
                                         </div>
